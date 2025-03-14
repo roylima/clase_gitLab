142d62e9297b255adffe67447188388d8528d4f2
roy4L <royalejan26@gmail.com>
2025-03-14 10:40:12 -0400

    Agregando operacion y saludo

---
+++ Operacion.py
@@ -0,0 +1,5 @@
+# Operacion Simple
+numero1=5
+numero2=10
+suma=numero1 +numero2
+print("La suma de ",numero1," y ",numero2," es: ",suma)
---
+++ Saludo.java
@@ -0,0 +1,11 @@
+public class Saludo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HOLA EQUIPO !!!");
+    }
+    
+}
